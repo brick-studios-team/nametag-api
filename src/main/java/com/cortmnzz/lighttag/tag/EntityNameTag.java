ae0b4f26e4d4a4cb4fa77ac2a7adec607a16c14a
Ailakks <hola@cortmnzz.com>
2023-03-26 01:01:45 +0100

    changed to spigot 1.8 nms packets

--- src/main/java/com/cortmnzz/lighttag/tag/EntityNameTag.java
+++ src/main/java/com/cortmnzz/lighttag/tag/EntityNameTag.java
@@ -1,14 +1,12 @@
 package com.cortmnzz.lighttag.tag;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.wrappers.EnumWrappers;
-import com.comphenix.protocol.wrappers.WrappedDataWatcher;
 import com.cortmnzz.lighttag.manager.NameTagManager;
-import org.bukkit.entity.ArmorStand;
+import net.minecraft.server.v1_8_R3.EntityArmorStand;
+import net.minecraft.server.v1_8_R3.PacketPlayOutAttachEntity;
+import net.minecraft.server.v1_8_R3.PacketPlayOutSpawnEntityLiving;
+import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
@@ -19,17 +17,16 @@ public class EntityNameTag {
     private final Entity entity;
     private final List<TagLine> tagLineList;
 
-    private PacketContainer packet;
-    private WrappedDataWatcher dataWatcher;
-
     public EntityNameTag(Entity entity) {
         this.entity = entity;
         this.tagLineList = new ArrayList<>();
     }
+
     public EntityNameTag addTagLine(Function<Player, String> function) {
         this.tagLineList.add(new TagLine(function));
         return this;
     }
+
     public void applyAll() {
         NameTagManager.doGlobally(this::apply);
     }
@@ -39,29 +36,19 @@ public class EntityNameTag {
             Player player = (Player) target;
 
             this.tagLineList.forEach(line -> {
-                packet = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.SPAWN_ENTITY_LIVING);
+                EntityArmorStand entityArmorStand = new EntityArmorStand(((CraftWorld) player.getWorld()).getHandle(),
+                        player.getLocation().getX(),
+                        player.getLocation().getY(),
+                        player.getLocation().getZ());
+                entityArmorStand.setInvisible(true);
+                entityArmorStand.setCustomNameVisible(true);
+                entityArmorStand.setCustomName(line.getText().apply(player));
 
-                ArmorStand armorStand = (ArmorStand) player.getWorld().spawnEntity(player.getLocation(), EntityType.ARMOR_STAND);
-                armorStand.setGravity(false);
-                armorStand.setSmall(true);
-                armorStand.setVisible(false);
-                armorStand.setMarker(true);
+                PacketPlayOutSpawnEntityLiving packetPlayOutSpawnEntityLiving = new PacketPlayOutSpawnEntityLiving(entityArmorStand);
+                PacketPlayOutAttachEntity packetPlayOutAttachEntity = new PacketPlayOutAttachEntity(0, entityArmorStand, ((CraftPlayer) player).getHandle());
 
-                this.dataWatcher = new WrappedDataWatcher();
-                WrappedDataWatcher.WrappedDataWatcherObject slimeSizeIndex = new WrappedDataWatcher.WrappedDataWatcherObject(15, WrappedDataWatcher.Registry.get(Byte.class));
-                dataWatcher.setObject(slimeSizeIndex, (byte) 2);
-
-                packet.getIntegers().write(0, armorStand.getEntityId());
-                packet.getHands().write(0, EnumWrappers.Hand.MAIN_HAND);
-                packet.getModifier().write(1, armorStand.getLocation().toVector());
-                packet.getBooleans().write(0, true);
-                packet.getDataWatcherModifier().write(0, dataWatcher);
-
-                try {
-                    ProtocolLibrary.getProtocolManager().sendServerPacket(player, packet);
-                } catch (Exception exception) {
-                    exception.printStackTrace();
-                }
+                ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packetPlayOutSpawnEntityLiving);
+                ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packetPlayOutAttachEntity);
             });
         }
     }
