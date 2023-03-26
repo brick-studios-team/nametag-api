e7a5fc3020baa44d3fe40502c2f6236c1470b8e5
Ailakks <hola@cortmnzz.com>
2023-03-26 21:22:09 +0200

    removed example nametag

--- src/main/java/com/cortmnzz/lighttag/listeners/bukkit/PlayerJoinListener.java
+++ src/main/java/com/cortmnzz/lighttag/listeners/bukkit/PlayerJoinListener.java
@@ -4,17 +4,15 @@ import com.cortmnzz.lighttag.manager.TagPlayerManager;
 import com.cortmnzz.lighttag.player.TagPlayer;
 import com.cortmnzz.lighttag.tag.EntityNameTag;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerJoinListener implements Listener {
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerJoin(PlayerJoinEvent event) {
         TagPlayer tagPlayer = TagPlayerManager.get(event.getPlayer());
 
         tagPlayer.applyAll(tagPlayer);
-
-        tagPlayer.setEntityNameTag(new EntityNameTag(tagPlayer.getBukkitPlayer()));
-        tagPlayer.getEntityNameTag().addTagLine(target -> "example").addTagLine(target -> "example2").applyAll(tagPlayer);
     }
 }
--- src/main/java/com/cortmnzz/lighttag/player/TagPlayer.java
+++ src/main/java/com/cortmnzz/lighttag/player/TagPlayer.java
@@ -11,19 +11,26 @@ package com.cortmnzz.lighttag.player;
 
 import com.cortmnzz.lighttag.manager.TagPlayerManager;
 import com.cortmnzz.lighttag.tag.EntityNameTag;
+import com.cortmnzz.lighttag.tag.NameTag;
 import lombok.Setter;
 import lombok.Getter;
+import lombok.experimental.Accessors;
 import org.bukkit.entity.Player;
 
+@Accessors(chain = true)
 public class TagPlayer {
     @Getter private final Player bukkitPlayer;
 
-    @Getter @Setter private EntityNameTag entityNameTag;
+    @Getter private EntityNameTag entityNameTag;
 
     public TagPlayer(Player bukkitPlayer) {
         this.bukkitPlayer = bukkitPlayer;
     }
     public void applyAll(TagPlayer tagPlayer) {
         TagPlayerManager.doGlobally(tagPlayer, target -> target.getEntityNameTag().apply(this.bukkitPlayer));
+    }
+    public EntityNameTag setEntityNameTag(EntityNameTag entityNameTag) {
+        this.entityNameTag = entityNameTag;
+        return this.entityNameTag;
     }
 }
